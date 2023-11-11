ba95a3efbca521b87e986f061429ee3a132cf04a
Pridacia <133506010+Pridacia@users.noreply.github.com>
2023-11-11 16:10:15 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Описание задания.
+        // 1 уровень сложности: 1. В методе main инициализировать все примитивные типы и вывести их результат в консоль:
+        //char: G
+        //int: 89
+        //byte: 4
+        //short: 56
+        //float: 4.7333436
+        //double: 4.355453532
+        //long: 12121
+        byte a = 4;
+        short b = 56;
+        int c = 89;
+        long d = 12121;
+        char e = 'G';
+        float f = 4.7333436f;
+        double g = 4.355453532;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}
